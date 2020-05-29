bb3b7600e80de7fdc57caffe3531017184b47dd6
Victor Godwin <conquerorsword36@yahoo.com>
2020-05-29 10:07:40 -0700

    new done

--- dist/database/config/config.js
+++ dist/database/config/config.js
@@ -3,16 +3,6 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    database: 'books',
-    username: 'me',
-    password: 'password',
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
-  test: {
-    use_env_variable: 'DATABASE_URL_TEST'
-  },
   production: {
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
